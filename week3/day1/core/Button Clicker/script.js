a6961aeb7e097f0272bdd4c5fee265702be2bc7d
HOUCEM-FADHL <houcem.fadhl@gmail.com>
2023-12-05 02:03:23 +0100

    first commit

---
+++ week3/day1/core/Button Clicker/script.js
@@ -0,0 +1,9 @@
+// Function to update the text content of an element to "Logout"
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+// Function to remove an element from the DOM
+function hide(element) {
+    element.remove();
+}
