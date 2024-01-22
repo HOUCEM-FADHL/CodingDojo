1dccbcacd3f2e4697153361874102405109ea3c8
HOUCEM-FADHL <houcem.fadhl@gmail.com>
2024-01-22 01:24:13 +0100

    organize with prettier

--- MERN Stack/MangoDB/Core/Jokes-API/server/config/mongoose.config.js
+++ MERN Stack/MangoDB/Core/Jokes-API/server/config/mongoose.config.js
@@ -1,12 +1,15 @@
 // Import the mongoose library
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to the MongoDB database using the specified connection string
-mongoose.connect('mongodb://127.0.0.1:27017/jokesDB', {
+mongoose
+  .connect("mongodb://127.0.0.1:27017/jokesDB", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    // If the connection is successful, log a message
-    .then(() => console.log('Established a connection to the database'))
-    // If an error occurs during the connection, log an error message
-    .catch(err => console.log('Something went wrong when connecting to the database ', err));
+    useUnifiedTopology: true,
+  })
+  // If the connection is successful, log a message
+  .then(() => console.log("Established a connection to the database"))
+  // If an error occurs during the connection, log an error message
+  .catch((err) =>
+    console.log("Something went wrong when connecting to the database ", err)
+  );
--- MERN Stack/MangoDB/Core/Jokes-API/server/controllers/joke.controller.js
+++ MERN Stack/MangoDB/Core/Jokes-API/server/controllers/joke.controller.js
@@ -1,55 +1,60 @@
 // Import the Joke model
-const Joke = require('../models/joke.model');
+const Joke = require("../models/joke.model");
 
 // Export an object with methods for handling Joke-related operations
 module.exports = {
     // Retrieve all jokes
     getAllJokes: (req, res) => {
         Joke.find()
-        .then(allJokes => res.json(allJokes))
-        .catch(err => res.status(500).json(err));
+        .then((allJokes) => res.json(allJokes))
+        .catch((err) => res.status(500).json(err));
     },
 
     // Retrieve a single joke by its ID
     getOneJoke: (req, res) => {
         Joke.find({ _id: req.params.id })
-        .then(oneJoke => res.json(oneJoke))
-        .catch(err => res.status(500).json(err));
+        .then((oneJoke) => res.json(oneJoke))
+        .catch((err) => res.status(500).json(err));
     },
 
     // Create a new joke
-    createJoke: (req,res) => {
+    createJoke: (req, res) => {
         const { setup, punchline } = req.body;
         Joke.create({ setup, punchline })
-        .then(() => res.json('Joke added!'))
-        .catch(err => res.status(500).json(err));
+        .then(() => res.json("Joke added!"))
+        .catch((err) => res.status(500).json(err));
     },
 
     // Update an existing joke by its ID
     updateJoke: (req, res) => {
-        Joke.findOneAndUpdate({ _id: req.params.id }, {
+        Joke.findOneAndUpdate(
+        { _id: req.params.id },
+        {
             setup: req.body.setup,
-            punchline: req.body.punchline
-        }, { new: true, runValidators: true })
-        .then(joke => res.json(joke))
-        .catch(err => res.status(500).json(err));
+            punchline: req.body.punchline,
+        },
+        { new: true, runValidators: true }
+        )
+        .then((joke) => res.json(joke))
+        .catch((err) => res.status(500).json(err));
     },
 
     // Delete a joke by its ID
     deleteJoke: (req, res) => {
         Joke.findOneAndDelete({ _id: req.params.id })
-        .then(() => res.json('Joke deleted.'))
-        .catch(err => res.status(500).json(err));
+        .then(() => res.json("Joke deleted."))
+        .catch((err) => res.status(500).json(err));
     },
 
     // Retrieve a random joke
     getRandomJoke: (req, res) => {
-        Joke.countDocuments()
-        .then(count => {
-             const random = Math.floor(Math.random() * count);
-             Joke.findOne().skip(random).exec()
-             .then(doc => res.json(doc))
-             .catch(err => res.status(500).json(err));
-        })
-    }
-}
+        Joke.countDocuments().then((count) => {
+        const random = Math.floor(Math.random() * count);
+        Joke.findOne()
+            .skip(random)
+            .exec()
+            .then((doc) => res.json(doc))
+            .catch((err) => res.status(500).json(err));
+        });
+    },
+};
