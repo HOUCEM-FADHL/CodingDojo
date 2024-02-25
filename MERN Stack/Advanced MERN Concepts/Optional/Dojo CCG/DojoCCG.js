2114b5e1a2369abab02df68e224dd8e0d9a540cf
HOUCEM-FADHL <houcem.fadhl@gmail.com>
2024-02-26 00:49:19 +0100

    it,s time to duel

--- MERN Stack/Advanced MERN Concepts/Optional/Dojo CCG/DojoCCG.js
+++ MERN Stack/Advanced MERN Concepts/Optional/Dojo CCG/DojoCCG.js
@@ -18,19 +18,21 @@ class Card {
     }
 
     class Effect extends Card {
-    constructor(name, cost, effect) {
+    constructor(name, cost, stat, magnitude) {
         super(name, cost);
-        this.effect = effect;
+        this.stat = stat;
+        this.magnitude = magnitude;
     }
     play(target) {
         if (target instanceof Unit) {
         // implement card text : "Raise / Lower the target's resilience / power by amount"
-        // target.power += this.effect;
-        target.res += this.effect;
-        if(this.effect<0){
-            console.log(`Lower ${target.name}'s resilience by ${-this.effect}`)
-        } else if(this.effect>0){
-        console.log(`Increase ${target.name}'s resilience by ${this.effect}`)
+        // target.power += this.magnitude;
+        if (this.stat==="resilience"){target.res += this.magnitude}
+        else if (this.stat==="power"){target.power += this.magnitude}
+        if(this.magnitude<0){
+            console.log(`Lower ${target.name}'s ${this.stat} by ${-this.magnitude}`)
+        } else if(this.magnitude>0){
+        console.log(`Increase ${target.name}'s ${this.stat} by ${this.magnitude}`)
         }
         } else {
         throw new Error("Target must be a unit!");
@@ -38,16 +40,23 @@ class Card {
     }
 }
 
-const card1 = new Unit ("Black Belt", 10,10,10)
-const card2 = new Unit ("Red Belt",10,10,15)
-const cardEffect1 = new Effect ("Hard Algorithm", 10, 3)
-const cardEffect2 = new Effect ("Easy Algorithm", 10, -3)
-console.log(`${card1.name} have ${card1.power} power and ${card1.res} resilience`)
-console.log(`${card2.name} have ${card2.power} power and ${card2.res} resilience`)
-card1.attack(card2)
-console.log(`${card1.name} have ${card1.power} power and ${card1.res} resilience`)
-console.log(`${card2.name} have ${card2.power} power and ${card2.res} resilience`)
-cardEffect1.play(card1)
-cardEffect2.play(card2)
-console.log(`${card1.name} have ${card1.power} power and ${card1.res} resilience`)
-console.log(`${card2.name} have ${card2.power} power and ${card2.res} resilience`)
+const Red = new Unit ("Red Belt",3,3,4)
+console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+const cardEffect1 = new Effect ("Hard Algorithm", 2,"resilience", 3)
+cardEffect1.play(Red)
+console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+const Black = new Unit ("Black Belt", 4,5,4)
+const cardEffect2 = new Effect ("Unhandled Promise Rejection", 1,"resilience", -2)
+cardEffect2.play(Red)
+console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+const cardEffect3 = new Effect ("Pair Programming", 3,"power", 2)
+cardEffect3.play(Red)
+console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+console.log(`${Black.name} have ${Black.power} power and ${Black.res} resilience`)
+Red.attack(Black)
+console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+console.log(`${Black.name} have ${Black.power} power and ${Black.res} resilience`)
+// console.log(`${Black.name} have ${Black.power} power and ${Black.res} resilience`)
+// console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
+// console.log(`${Black.name} have ${Black.power} power and ${Black.res} resilience`)
+// console.log(`${Red.name} have ${Red.power} power and ${Red.res} resilience`)
